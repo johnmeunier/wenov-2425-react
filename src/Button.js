f6535359cc13c0fa9aeda86eaf4e5438a99a4269
Johnathan MEUNIER <johnathan2693@gmail.com>
2024-10-07 14:51:37 +0200

    example useState et useEffect

--- src/Button.js
+++ src/Button.js
@@ -1,13 +1,31 @@
-const Button = ({ children }) => (
-  <button
-    type="button"
-    className="wenov-button"
-    onClick={() => {
-      console.log("click");
-    }}
-  >
-    {children}
-  </button>
-);
+import { useState, useEffect } from "react";
+
+const Button = ({ children }) => {
+  const [count, setCount] = useState(0);
+  const [toto, setToto] = useState("");
+
+  useEffect(() => {
+    console.log(count);
+  }, []);
+
+  return (
+    <>
+      <button
+        type="button"
+        className="wenov-button"
+        onMouseEnter={() => {
+          setToto(new Date());
+        }}
+        onClick={() => {
+          setCount((currentCount) => currentCount + 1);
+        }}
+      >
+        {children}
+      </button>
+      <h3>Mon bouton a été cliqué {count} fois</h3>
+      <h3>{toto.valueOf()}</h3>
+    </>
+  );
+};
 
 export default Button;
