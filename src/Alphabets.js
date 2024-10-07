ac2f8fd1b64c455c5c4d5e7f99f6463831adbd5b
Johnathan MEUNIER <johnathan2693@gmail.com>
2024-10-07 10:37:50 +0200

    premier composant alphabets

---
+++ src/Alphabets.js
@@ -0,0 +1,40 @@
+const Alphabets = () => {
+  const letters = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+
+  return (
+    <ul>
+      {letters.map((letter) => (
+        <li>{letter}</li>
+      ))}
+    </ul>
+  );
+};
+
+export default Alphabets;
